f5453097764e0c7f04078de51c6b7aa44964aa11
Matthieu LEFEVRE <matth.lfvr@gmail.com>
2022-03-07 14:15:14 +0100

    Changement de materiel réussi

--- CarcaSpace/Assets/AccessDenied.cs
+++ CarcaSpace/Assets/AccessDenied.cs
@@ -7,17 +7,33 @@ public class AccessDenied : MonoBehaviour
 {
     GameObject face;
     List<Material> m;
+    bool animDenied;
+    bool endAnimDenied;
+    Material Cross;
+    Material Red;
+    Material logo;
     // Start is called before the first frame update
     void Start()
     {
+      Cross = Resources.Load<Material>("Material/Cross");
+      Red = Resources.Load<Material>("Material/Red");
+      logo = Resources.Load<Material>("Material/logo");
       face = gameObject.transform.GetChild(2).gameObject;
       m = face.GetComponent<Renderer>().materials.ToList();
-
     }
 
     // Update is called once per frame
     void Update()
     {
+
+      if (Input.GetKey("m") && !(animDenied && endAnimDenied))
+          animDenied = true;
+
+      if (animDenied)
+      {
+        m[1] = Red;
+        face.GetComponent<Renderer>().materials = m.ToArray();
+      }
 
     }
 }
