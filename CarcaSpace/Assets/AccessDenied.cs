a7669688f716d7a965794a6e6e164d0bcff5a2e8
Matthieu LEFEVRE <matth.lfvr@gmail.com>
2022-03-07 11:53:35 +0100

    Array to List

--- CarcaSpace/Assets/AccessDenied.cs
+++ CarcaSpace/Assets/AccessDenied.cs
@@ -1,22 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class AccessDenied : MonoBehaviour
 {
     GameObject face;
-    Material[] m;
+    List<Material> m;
     // Start is called before the first frame update
     void Start()
     {
       face = gameObject.transform.GetChild(2).gameObject;
-      m = face.GetComponent<Renderer>().materials;
-      Debug.Log(m[0]);
+      m = face.GetComponent<Renderer>().materials.ToList();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
