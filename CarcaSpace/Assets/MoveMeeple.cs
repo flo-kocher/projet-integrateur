2c41ac99be457b56787b52b745273510e360108a
Matthieu LEFEVRE <matth.lfvr@gmail.com>
2022-03-22 13:47:46 +0100

    alignement réussi

--- CarcaSpace/Assets/MoveMeeple.cs
+++ CarcaSpace/Assets/MoveMeeple.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class MoveMeeple : MonoBehaviour
 {
     // Start is called before the first frame update
+    public bool nord;
+    public bool sud;
+    public bool est;
+    public bool ouest;
     void Start()
     {
 
@@ -13,8 +17,21 @@ public class MoveMeeple : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        nord = false;
+        sud = false;
+        est = false;
+        ouest = false;
         float x = transform.position.x-(transform.position.x%0.33f);
         float y = transform.position.y-(transform.position.y%0.33f);
-        transform.position = new Vector2(x+0.17f,y+0.17f);
+        if (y<0.33f)
+          sud = true;
+        if (y>=0.66f)
+          nord = true;
+        if (x<0.33f)
+          ouest = true;
+        if (x>=0.66f) 
+          est = true;
+        if (!((sud && ouest) || (sud && est) || (nord && ouest) || (nord && est)))
+           transform.position = new Vector2(x+0.17f,y+0.17f); 
     }
 }
