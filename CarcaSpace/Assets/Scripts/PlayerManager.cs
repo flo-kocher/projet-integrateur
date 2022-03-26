f0521154cba86a75f0e2a378061ee6fc155f47c9
Rana Abou saab <chamaael@etu.unistra.fr>
2022-03-26 18:52:55 +0100

    Instantiation tuiles Correcte

--- CarcaSpace/Assets/Scripts/PlayerManager.cs
+++ CarcaSpace/Assets/Scripts/PlayerManager.cs
@@ -30,14 +30,156 @@ public class PlayerManager : NetworkBehaviour
         ui = GameObject.Find("UI");
     }
 
-    
+     public List<GameObject>  instatiateTiles(){
+        GameObject go = GameObject.Find("Temp");
+        GameObject tmp = GameObject.Instantiate(temp);
+        int x = 0 ;
+        for(int i =1 ;i< 25; i++){
+            switch(i){
+                case 1 : 
+                   tmp.AddComponent<tile_type_1>();
+                   //go  = GameObject.Instantiate(tmp);
+                   all_tiles.Add(tmp);
+                   break;
+                case 2 : 
+                   tmp.AddComponent<tile_type_2>();
+                   all_tiles.Add(tmp);
+                   all_tiles.Add(tmp);
+                   break;
+                case 3 : 
+                   tmp.AddComponent<tile_type_3>();
+                   all_tiles.Add(tmp);
+                   all_tiles.Add(tmp);
+                   all_tiles.Add(tmp);
+                   all_tiles.Add(go);
+                   break;
+                case 4 : 
+                   tmp.AddComponent<tile_type_4>();
+                   all_tiles.Add(tmp);
+                   all_tiles.Add(tmp);
+                   all_tiles.Add(tmp);
+                   break;
+                case 5 : 
+                   tmp.AddComponent<tile_type_5>();
+                   all_tiles.Add(tmp);
+                   break;
+                case 6 : 
+                   tmp.AddComponent<tile_type_6>();
+                   all_tiles.Add(tmp);
+                   break;
+                case 7 : 
+                   tmp.AddComponent<tile_type_7>();
+                   all_tiles.Add(tmp);
+                   all_tiles.Add(tmp);
+                   break;
+                case 8 : 
+                   tmp.AddComponent<tile_type_8>();
+                   all_tiles.Add(tmp);
+                   all_tiles.Add(tmp);
+                   all_tiles.Add(tmp);
+                   break;
+                case 9 : 
+                   tmp.AddComponent<tile_type_9>();
+                   all_tiles.Add(tmp);
+                   all_tiles.Add(tmp);
+                   break;
+                case 10 : 
+                   tmp.AddComponent<tile_type_10>();
+                   all_tiles.Add(tmp);
+                   all_tiles.Add(tmp);
+                   break;
+                case 11 : 
+                   tmp.AddComponent<tile_type_11>();
+                   all_tiles.Add(tmp);
+                   all_tiles.Add(tmp);
+                   all_tiles.Add(tmp);
+                   break;
+                case 12 : 
+                   tmp.AddComponent<tile_type_12>();
+                   all_tiles.Add(tmp);
+                   all_tiles.Add(tmp);
+                   break;
+                case 13 : 
+                   tmp.AddComponent<tile_type_13>();
+                   all_tiles.Add(tmp); 
+                   break;                       
+                case 14 :
+                    for(x=0;x<1;x++){
+                        tmp.AddComponent<tile_type_14>();
+                        all_tiles.Add(tmp);
+                    }
+                    break;
+                case 15 :
+                    for(x=0;x<2;x++){
+                        tmp.AddComponent<tile_type_15>();
+                        all_tiles.Add(tmp);
+                    }
+                    break;
+                case 16 :
+                    for(x=0;x<4;x++){
+                        tmp.AddComponent<tile_type_16>();
+                        all_tiles.Add(tmp);
+                    }
+                    break;
+                case 17 :
+                    for(x=0;x<2;x++){
+                        tmp.AddComponent<tile_type_17>();
+                        all_tiles.Add(tmp);
+                    }
+                    break;
+                case 18 :
+                    for(x=0;x<2;x++){
+                        tmp.AddComponent<tile_type_18>();
+                        all_tiles.Add(tmp);
+                    } 
+                    break;
+                case 19 :
+                    for(x=0;x<2;x++){
+                        tmp.AddComponent<tile_type_19>();
+                        all_tiles.Add(tmp);
+                    } 
+                    break;
+                //a revoir
+                case 20 :
+                    for(x=0;x<3;x++){
+                        tmp.AddComponent<tile_type_20>();
+                        all_tiles.Add(tmp);
+                    } 
+                    break;
+                case 21 :
+                    tmp.AddComponent<tile_type_21>();
+                    all_tiles.Add(tmp);
+                    break;
+                case 22 :
+                    for(x=0;x<3;x++){
+                        tmp.AddComponent<tile_type_22>();
+                        all_tiles.Add(tmp);
+                    }  
+                    break;
+                case 23:
+                  for(x=0;x<7;x++){
+                        tmp.AddComponent<tile_type_23>();
+                        all_tiles.Add(tmp);
+                    }  
+                    break;
+                case 24 :
+                    for(x=0;x<8;x++){
+                        tmp.AddComponent<tile_type_24>();
+                        all_tiles.Add(tmp);
+                    }
+                    break;
+            }
+                
+        }
+        return all_tiles;
+    }
 
 
     [Server]
     public override void OnStartServer()
     {
         base.OnStartServer();
-        all_tiles= GetComponent<TileManager>().instatiateTiles();
+        instatiateTiles();
         Debug.Log("els dans all_tiles : " +all_tiles);
     }
 
@@ -66,7 +208,7 @@ public class PlayerManager : NetworkBehaviour
         {
             randInt=rnd.Next(0,all_tiles.Count);
             // on pioche la tuile dans la liste all-tiles et puis on la supprime de la liste
-            GameObject tuilos = Instantiate(all_tiles[randInt]);
+            GameObject tuilos = all_tiles[randInt];
             all_tiles.RemoveAt(randInt);
             NetworkServer.Spawn(tuilos, connectionToClient);
             RpcShowTiles(tuilos, "Dealt");
