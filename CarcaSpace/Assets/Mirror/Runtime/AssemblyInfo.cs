1c746cc67cbb436f04a1f0d136b2372fc28ee236
Rana Abou saab <chamaael@etu.unistra.fr>
2022-04-07 14:32:50 +0200

    Fix package Mirror

--- CarcaSpace/Assets/Mirror/Runtime/AssemblyInfo.cs
+++ CarcaSpace/Assets/Mirror/Runtime/AssemblyInfo.cs
@@ -2,12 +2,9 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Mirror.Tests.Common")]
 [assembly: InternalsVisibleTo("Mirror.Tests")]
-<<<<<<< HEAD
 // need to use Unity.*.CodeGen assembly name to import Unity.CompilationPipeline
 // for ILPostProcessor tests.
 [assembly: InternalsVisibleTo("Unity.Mirror.Tests.CodeGen")]
-=======
->>>>>>> origin/alpha_merge
 [assembly: InternalsVisibleTo("Mirror.Tests.Generated")]
 [assembly: InternalsVisibleTo("Mirror.Tests.Runtime")]
 [assembly: InternalsVisibleTo("Mirror.Tests.Performance.Editor")]
---
+++ CarcaSpace_clone_0/Assets/Tiles/tile_type.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror ;
+
+
+
+
+public abstract class tile_type : NetworkBehaviour
+{
+
+    
+    
+    public Type_land haut;
+    public Type_land gauche;
+    public Type_land bas;
+    public Type_land droite;
+    public Type_land milieu;
+    public int coordX;
+    public int coordY;
+    public rotateZ r;
+    public static int nbrTuile = 30;
+    public static bool finish = false;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = this.GetComponent<rotateZ>();
+        haut = Type_land.Plaine;
+        gauche = Type_land.Chemin;
+        bas = Type_land.Plaine;
+        droite = Type_land.Chemin;
+        milieu = Type_land.Chemin;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("left") && r!=null)
+        {
+            if(!(r.leve || r.couche || r.tourne)){
+                rotate_left();
+            }
+            
+        }
+        if(Input.GetKeyDown("right") && r!=null)
+        {
+            if(!(r.leve || r.couche || r.tourne)){
+                rotate_right();
+            }
+        }
+    }
+
+    protected void rotate_left()
+    {
+        Type_land cp = haut;
+        haut = droite;
+        droite = bas;
+        bas = gauche;
+        gauche = cp;
+    }
+
+    protected void rotate_right()
+    {
+        Type_land cp = haut;
+        haut = gauche;
+        gauche = bas;
+        bas = droite;
+        droite = cp;
+    }
+
+    public abstract int getNbrTuile();
+
+    public abstract void decrementNbrTuile();
+
+    public abstract bool getFinish();
+
+    public abstract void changeFinish();
+    
+
+}
