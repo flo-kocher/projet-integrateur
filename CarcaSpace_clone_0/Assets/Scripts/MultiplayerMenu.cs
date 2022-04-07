a9536c06e99bb3421141604256af0da58eb7ac08
Rana Abou saab <chamaael@etu.unistra.fr>
2022-04-07 17:02:29 +0200

    Avancement dans les scenes Creating et joining

--- CarcaSpace_clone_0/Assets/Scripts/MultiplayerMenu.cs
+++ CarcaSpace_clone_0/Assets/Scripts/MultiplayerMenu.cs
@@ -8,8 +8,7 @@ public class MultiplayerMenu : MonoBehaviour
 {
     private Button Create;
     private Button JoinFriends;
-    private Button addPlayers;
-    private Button subPlayers;
+    
     private Text text;
     private Mask mask;
     public void EnterMultiplayerMenu()
@@ -26,42 +25,16 @@ public class MultiplayerMenu : MonoBehaviour
     {
         SceneManager.LoadScene("JoiningMenu",LoadSceneMode.Single);
     }
-    public void addPlayer()
-    {
-        text = GameObject.Find("PlayerInput").GetComponent<Text>();
-        mask = GameObject.Find("left-arrow").GetComponent<Mask>();
-
-        
-        int nbPlayers = int.Parse(text.text) + 1;
-        if(nbPlayers == 2){
-            mask.showMaskGraphic = true;
-        }
-        text.text = nbPlayers.ToString();
-    }
-
-    public void substractPlayer()
-    {
-        text = GameObject.Find("PlayerInput").GetComponent<Text>();
-        mask = GameObject.Find("left-arrow").GetComponent<Mask>();
-
-        int nbPlayers = int.Parse(text.text);
-
-        if(nbPlayers > 1)
-        {
-            nbPlayers -= 1;
-            if(nbPlayers == 1)
-            {
-                mask.showMaskGraphic = false;
-            }
-        }
-        
-        text.text = nbPlayers.ToString();
-    }
+    
     void Start() {
-        Create = GameObject.Find("Create Game").GetComponent<Button>();
+        Create = GameObject.Find("CreateGame").GetComponent<Button>();
         JoinFriends = GameObject.Find("JoinFriends").GetComponent<Button>();
-        
+        // addPlayers = GameObject.Find("right-arrow").GetComponent<Button>();
+        // subPlayers = GameObject.Find("left-arrow").GetComponent<Button>();
+
         Create.onClick.AddListener(EnterCreateMenu);
         JoinFriends.onClick.AddListener(EnterJoinFriendsMenu);
+        // addPlayers.onClick.AddListener(addPlayer);
+        // subPlayers.onClick.AddListener(substractPlayer);
     }
 }
