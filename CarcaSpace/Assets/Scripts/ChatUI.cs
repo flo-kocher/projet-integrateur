f432fb79ce31c366d1d0dcfcb1330892a3d25292
Rana Abou saab <chamaael@etu.unistra.fr>
2022-05-02 20:16:00 +0200

    ajout scripts lobby new

---
+++ CarcaSpace/Assets/Scripts/ChatUI.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using UnityEngine.UI;
+using TMPro;
+using Mirror;
+
+
+public class ChatUI : NetworkBehaviour
+{
+
+
+    [SerializeField] public GameObject chatUI = null;
+    [SerializeField] public TMP_Text chatText = null;
+    [SerializeField] public TMP_InputField inputField = null;
+
+
+    private static event Action<string> OnMessage;
+
+    public override void OnStartAuthority()
+    {
+        chatUI.SetActive(true);
+
+        OnMessage += HandleNewMessage;
+    }
+
+    [ClientCallback]
+    private void OnDestroy()
+    {
+        if (!hasAuthority) { return; }
+
+        OnMessage -= HandleNewMessage;
+    }
+
+    private void HandleNewMessage(string message)
+    {
+        chatText.text += message;
+    }
+
+    [Client]
+    public void Send(string message)
+    {
+        message = inputField.text ;
+        Debug.Log($"sending {message}");
+        Debug.Log("sending");
+
+        if (!Input.GetKeyDown(KeyCode.Return)) { 
+            Debug.Log("ici 1 ");
+            return; 
+        }
+
+        if (string.IsNullOrWhiteSpace(message)) { 
+            Debug.Log("ici 2");
+            return; 
+        }
+        CmdSendMessage(message);
+        inputField.text = string.Empty;
+    }
+
+    [Command]
+    private void CmdSendMessage(string message)
+    {
+        RpcHandleMessage($"[{connectionToClient.connectionId}]: {message}");
+    }
+
+    [ClientRpc]
+    private void RpcHandleMessage(string message)
+    {
+        OnMessage?.Invoke($"\n{message}");
+    }
+}
---
+++ CarcaSpace/Assets/Scripts/UIplayer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIplayer : MonoBehaviour
+{
+
+    //SerializeField] Text text;
+
+    //PlayerManager player; //direct reference to player script
+
+    //public void SetPlayer (PlayerManager player) {
+    //    this.player = player;
+    //    text.text = "Player " + PlayerManager.playerIndex.ToString();
+
+
+   // }
+
+}
